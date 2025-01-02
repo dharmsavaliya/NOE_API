03d3d6d2bf3cab9a42ab48c95c89c2cee3703dff
dharmsavaliya <dharmsavaliya5@gmail.com>
2025-01-02 09:40:21 +0530

    fisrt project for displaly name

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=> res.send("Hello, world!"));
+app.get('/student', (req, res)=> res.send("dharm savaliya"));
+app.get('/uni', (req, res)=> res.send("RKU"));
+app.get('/collage', (req, res)=> res.send("SOE"));
+
+
 app.listen(port,()=>console.log('example listening on port ${port}!'));
+
+
+// create one end point/student to disaply student name / uni to disaply univarsity name / collage to disaply the collage name
